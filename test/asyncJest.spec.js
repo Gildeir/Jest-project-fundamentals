f4f87005863b72af758e6f7d3fa0da56d2ba9c92
Gildeir <gildeirlopes@gmail.com>
2021-04-23 22:49:20 -0300

    Acrescentando referencias

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -13,13 +13,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', async () => {
-    // expect.assertion(1);
     const answer = await answerPhone('Oi!');
     expect(answer).toBe('Oi!');
   });
 
   test('ocupado', async () => {
-    // expect.assertion(1);
+    // parte do código tirado de https: //jestjs.io/pt-BR/docs/asynchronous
+    // função toThrow tirado do material trybe : https://app.betrybe.com/course/fundamentals/testes-automatizados-com-jest/primeiros-passos-no-jest/28999aac-68b5-4aca-8c58-f9356ad8a00f/conteudos/e7d63c82-6e88-45fa-87fa-478f85168f58/expect-e-matchers/0fd0f1fb-e83e-4704-a9a6-e4e71d44ee02?use_case=previous_button
     const fail = 'Infelizmente não podemos atender...';
     await expect(answerPhone()).rejects.toThrowError(fail);
   });
