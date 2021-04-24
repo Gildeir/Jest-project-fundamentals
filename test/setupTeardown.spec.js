e939377d4ae7aa80b7320b1fe8e0ea235cbe6633
Gildeir <gildeirlopes@gmail.com>
2021-04-24 01:41:49 -0300

    Requisito setupTearDown

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -18,10 +18,15 @@ PS: Os codinomes dos aventureiros são reais! Tentem descobrir quem é quem!
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
-
+// Código baseado no plantão de revisão jest com Alexandre Trybo 10-A
 describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
+  beforeEach(() => {
+    adventure.randomAttack();
+  });
 
+  // afterEach(() => {
+  //   console.log(adventure.specialists);
+  // });
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
